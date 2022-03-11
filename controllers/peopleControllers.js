4bde4879b34c3541125b010c781e4fd6e0512cfd
alesalsa10 <alexandersalsa10@gmail.com>
2022-03-11 10:31:44 -0500

    added popular people route

--- controllers/peopleControllers.js
+++ controllers/peopleControllers.js
@@ -28,7 +28,22 @@ const getCredits = async(req, res) =>{
     }
 }
 
+const getPopular = async (req, res) => {
+  const { personId, mediaType } = req.params;
+  try {
+    const response = await axios.get(
+      `${process.env.baseUrl}/person/popular?api_key=${process.env.apiKey}`
+    );
+    console.log(response);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Msg: 'Something went wrong, try again later' });
+  }
+};
+
 module.exports = {
   getPersonById,
-  getCredits
+  getCredits,
+  getPopular
 };
--- routes/peopleRoutes.js
+++ routes/peopleRoutes.js
@@ -5,5 +5,6 @@ const peopleRouter = require('../controllers/peopleControllers');
 
 router.get('/:personId', peopleRouter.getPersonById);
 router.get('/:personId/:mediaType', peopleRouter.getCredits);
+router.get('/person/popular', peopleRouter.getPopular)
 
 module.exports = router;
