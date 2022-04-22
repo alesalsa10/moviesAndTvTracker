d866f76b1de0d8e4607ff6840f12e8c7a41565eb
alesalsa10 <alexandersalsa10@gmail.com>
2022-04-22 15:15:11 -0400

    auth

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -9,6 +9,10 @@ const options = {
   },
 };
  
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://www.broadmediacenter.com'
+    : 'http://localhost:3001';
 
 const sendEmail = async (
   email,
@@ -19,20 +23,20 @@ const sendEmail = async (
     const transporter = nodemailer.createTransport(sgTransport(options))
     if (emailType === 'verify') {
       await transporter.sendMail({
-        from: process.env.email,
+        from:  'Broad Media Center ' + process.env.EMAIL,
         to: email,
         subject: 'Please, verify your email',
         html: `
-        <p>Click <a href="http://localhost:3001/verify/${token}">Here</a> to verify your email</p>
+        <p>Click <a href="${baseUrl}/verify/${token}">Here</a> to verify your email</p>
       `,
       });
     } else {
       await transporter.sendMail({
-        from: process.env.email,
+        from: 'Broad Media Center ' + process.env.EMAIL,
         to: email,
         subject: 'Password reset',
         html: `
-        <p>Click <a href="http://localhost:3001/forgotPassword/${token}">Here</a> to reset password</p>
+        <p>Click <a href="${baseUrl}/forgotPassword/${token}">Here</a> to reset password</p>
       `,
       });
     }
